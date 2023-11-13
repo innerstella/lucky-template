2151a54edf7700ae25d51c66df0bb239e30222f7
innerstella <suji0509@naver.com>
2023-11-13 17:04:54 +0900

    feat : 초기화 기능

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,22 +13,34 @@ const MainPage = () => {
     storageCompany && setCompany(storageCompany);
   }, []);
 
+  const resetContent = () => {
+    if (window.confirm("초기화하시겠습니까?")) {
+      localStorage.clear();
+      window.location.reload();
+    }
+  };
+
   return (
     <MainContainer>
       {/* 로고 */}
       <Logo />
-      {/* 기업명 */}
-      <label className="input-company">🏢</label>
-      <input
-        type="text"
-        className="input-company"
-        placeholder="기업명을 입력하세요"
-        value={company}
-        onChange={(e) => {
-          setCompany(e.target.value);
-          localStorage.setItem("company", e.target.value);
-        }}
-      />
+      <div className="flex-row">
+        {/* 기업명 */}
+        <label className="input-company">🏢</label>
+        <input
+          type="text"
+          className="input-company"
+          placeholder="기업명을 입력하세요"
+          value={company}
+          onChange={(e) => {
+            setCompany(e.target.value);
+            localStorage.setItem("company", e.target.value);
+          }}
+        />
+        <div className="button" onClick={() => resetContent()}>
+          <p className="text">초기화하기</p>
+        </div>
+      </div>
       {/* 내용 */}
       <div className="content-box">
         <Content questionNum={1} />
@@ -61,5 +73,30 @@ const MainContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 2rem;
+  }
+  .flex-row {
+    display: flex;
+    flex-direction: row;
+  }
+  .button {
+    border-radius: 0.625rem;
+    border: 1px solid #000;
+    background: #646464;
+    height: 40px;
+    padding: 0.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .text {
+      color: #fff;
+      font-family: SUIT Variable;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      text-align: center;
+      margin: 0;
+    }
   }
 `;
